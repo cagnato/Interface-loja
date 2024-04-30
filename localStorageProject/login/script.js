1925679f21721444d59b5e8c47d99e290635e132
Davi Cagnato <113918935+cagnato@users.noreply.github.com>
2024-04-30 20:02:56 -0300

    Add files via upload

---
+++ localStorageProject/login/script.js
@@ -0,0 +1,44 @@
+var cont = 0;
+
+const showPassword = () => {
+    var inputSenha = document.querySelector("#senha");
+    // if(!((cont++)%2)) {inputSenha.setAttribute("type", "text")}
+    // else {inputSenha.setAttribute("type", "password");}
+    if(inputSenha.getAttribute("type") === "password"){
+        inputSenha.setAttribute("type", "text")
+    }else{
+        inputSenha.setAttribute("type", "password")
+    }
+}
+
+function formatarData(item){
+    var options = {
+        month: "numeric",
+        day: "numeric",
+        hour: "numeric",
+        minute: "numeric",
+        second: "numeric"
+    }
+
+    return item.toLocaleString("pt-BR", options)
+}
+
+function login(){
+    var nome = $("#nome").val();
+    var senha = $("#senha").val();
+
+    if(nome && senha && nome === "admin" && senha === "12345"){
+        const user = {
+            name: nome,
+            dataEntrada: formatarData(new Date()),
+            id: Math.floor(Math.random() * 100000),
+        }
+        localStorage.setItem("usuario", JSON.stringify(user))
+        window.location.href = "../loja"
+    }else{
+        document.getElementById("error-modal").style.display = "flex";
+    }
+    console.log(nome && senha && nome === "admin" && senha === "12345")
+}
+
+const fecharError = () =>   document.getElementById("error-modal").style.display = "none";
